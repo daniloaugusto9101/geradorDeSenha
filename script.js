f1e72ffe8d8404848c29cacecd916b3c26f2d007
Danilo Augusto <danilloagt@gmail.com>
2022-08-02 04:45:13 -0300

    html

---
+++ script.js
@@ -0,0 +1,12 @@
+const containerIcos = document.querySelector(".container-icos");
+const icoCopiar = document.querySelector(".ico-copiar");
+const icoCopiarOk = document.querySelector(".ico-copiar-ok");
+
+const btn = document.querySelector(".btn")
+
+
+containerIcos.addEventListener("click", function(){
+    icoCopiar.style.display = "hi";
+    icoCopiarOk.style.display = "block";
+    // console.log("claido");
+})
