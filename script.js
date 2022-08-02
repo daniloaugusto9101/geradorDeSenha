bb8dae105b86cfe1b17bf4c9b352bbbc1c191964
Danilo Augusto <danilloagt@gmail.com>
2022-08-02 05:46:52 -0300

    Versao 1.0

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const containerIcos = document.querySelector(".container-icos");
 const icoCopiar = document.querySelector(".ico-copiar");
 const icoCopiarOk = document.querySelector(".ico-copiar-ok");
-
+const range = document.querySelector(".range");
 const btn = document.querySelector(".btn")
+const texto = document.querySelector(".texto")
+const icoSenha = document.querySelector(".ico-senha")
 
 
-containerIcos.addEventListener("click", function(){
-    icoCopiar.style.display = "hi";
-    icoCopiarOk.style.display = "block";
-    // console.log("claido");
+const chave = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ!@#$%&*";
+
+// Texto total caracter
+range.addEventListener("input", function(){
+    texto.innerHTML = this.value;
 })
+
+// efeito copiar
+containerIcos.addEventListener("click", function(){
+    icoCopiar.style.display = "none";
+    icoCopiarOk.style.display = "block";
+    navigator.clipboard.writeText(icoSenha.innerHTML);
+})
+
+// Gerador de senha
+btn.addEventListener("click", function(){
+    let pass ="";
+    n = chave.length;
+    for (let i = 0; i < range.value; i++) {
+        pass += chave.charAt(Math.floor(Math.random() * n))
+    }
+    icoSenha.innerHTML = pass;
+
+    icoCopiar.style.display = "block";
+    icoCopiarOk.style.display = "none";
+})
+
